edae06865d4c75da00d1c56c779849fca81ec1f4
antaine <aconn@outlook.com>
2020-12-09 20:49:55 +0000

    Sing up links to User Table

--- AdventurersInn/src/app/signup/signup.component.ts
+++ AdventurersInn/src/app/signup/signup.component.ts
@@ -11,21 +11,45 @@ import { CrudService } from '../services/crudservice';
 export class SignupComponent implements OnInit {
 //Initialize
   isSignedIn = false
-
+  users: any;
+  userEmail: string;
   constructor(public crudService: CrudService,
     private router: Router,
     private app: AppComponent) { }
 
   ngOnInit(): void {
+    this.crudService.read_Users().subscribe(data => {
+      this.users = data.map(e => {
+        return {
+          id: e.payload.doc.id,
+          isEdit: false,
+          Email: e.payload.doc.data()['Email'],
+        };
+      })
+    });
   }
 
   //Sign Up Method
   async onSignUp(email:string,password:string){
     await this.crudService.signUp(email,password)
     if(this.crudService.isLoggedIn)
+    this.userEmail = email;
+    this.CreateUser();
     this.isSignedIn = true
     this.app.checkLogIn()
     this.router.navigate(['/home'])
+  }
+
+  CreateUser() {
+    let record = {};
+    record['Email'] = this.userEmail;
+    this.crudService.create_NewUser(record).then(resp => {
+      this.userEmail = "";
+      console.log(resp);
+    })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
 }
