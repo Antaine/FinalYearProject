806cc455bc4c83ea15d0ad8b420859c9e603ebbf
Colin Roche <G00349215@gmit.ie>
2021-03-04 14:35:38 +0000

    Adding character list to the profile page

--- AdventurersInn/src/app/profile/profile.component.ts
+++ AdventurersInn/src/app/profile/profile.component.ts
@@ -9,13 +9,52 @@ import { CrudService } from '../services/crudservice';
 })
 export class ProfileComponent implements OnInit {
 
+  //Variables
+  users: any;
+  email: string;
+
   constructor(public crudService: CrudService,
     private router: Router) { }
 
   ngOnInit(): void {
+    //Initialize
+    this.crudService.read_Characters().subscribe(data => {
+      //User Table
+      this.users = data.map(e => {
+        return {
+          id: e.payload.doc.id,
+          isEdit: false,
+          Name: e.payload.doc.data()['Name'],
+          Level: e.payload.doc.data()['Level'],
+          Character: e.payload.doc.data()['Character'],
+        };
+      })
+    })
+    if(localStorage.getItem('user') !== null){
+      this.email = localStorage.getItem('uEmail');}
   }
 
   create(){
     this.router.navigate(['/createcharacter'])
+  }
+
+  //Delete from Database
+  RemoveRecord(rowID) {
+    this.crudService.delete_User(rowID);
+  }
+//Update Record
+  EditRecord(record) {
+    record.isEdit = true;
+    record.EditName = record.Name;
+    record.EditLevel = record.Level;
+    record.EditCharacter = record.Character;
+  }
+  //Push Record
+  UpdateRecord(recordRow) {
+    let record = {};
+    record['Character'] = recordRow.EditCharacter;
+    record['Level'] = recordRow.EditLevel;
+    this.crudService.update_User(recordRow.id, record);
+    recordRow.isEdit = false;
   }
 }
