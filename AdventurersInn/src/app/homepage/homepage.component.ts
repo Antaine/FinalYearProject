f2585f4fdff1fecc282e503d3777b3e87bb329fd
Colin Roche <G00349215@gmit.ie>
2021-03-18 12:56:31 +0000

    Moving of code in homepage to forums, cleaning of code and styling

--- AdventurersInn/src/app/homepage/homepage.component.ts
+++ AdventurersInn/src/app/homepage/homepage.component.ts
@@ -22,59 +22,5 @@ export class HomepageComponent implements OnInit {
   constructor(public crudService: CrudService) { }
 
   ngOnInit(): void {
-    //Initialize
-    this.email = localStorage.getItem('uEmail');
-    this.crudService.read_Forums().subscribe(data => {
-          //Fourm Table
-          this.forums = data.map(e => {
-            return {
-              id: e.payload.doc.id,
-              isEdit: false,
-              PostName: e.payload.doc.data()['PostName'],
-              PostContent: e.payload.doc.data()['PostContent'],
-            };
-          })
-        });
   }
-
-  //Create Post Method
-  CreatePost(postData:{postName: string; postContent: string}) {
-    console.log(postData);
-    let record = {};
-    record['PostName'] =this.email;
-    record['PostContent'] = this.postContent;
-    this.crudService.post_Forum(record).then(resp => {
-      this.postName = "";
-      this.postContent = "";
-      console.log(resp);
-    })
-      .catch(error => {
-        console.log(error);
-      });
-  }
-
-  //Delete from Database
-  RemovePost(rowID) {
-    console.log("RemovePost: "+rowID);
-    this.crudService.delete_Post(rowID);
-  }
-  
-  //Update Record
-  EditPost(record) {
-    console.log("EditPost: "+record.toString());
-    record.isEdit = true;
-    record.EditPost = record.PostContent;
-    record.EditName = record.PostName;
-  }
-
-  //Push Record
-  UpdatePost(recordRow) {
-    let record = {};
-    record['PostContent'] = recordRow.PostContent;
-    record['PostName'] = recordRow.PostName;
-    console.log(recordRow.EditPost);
-    this.crudService.edit_Post(recordRow.id, record);
-    recordRow.isEdit = false;
-  }
-
 }
